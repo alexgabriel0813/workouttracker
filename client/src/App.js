6f487c4d72588b5886e1769f4d90883df3d165dc
Alex Gabriel <alexgabriel0813@gmail.com>
2018-10-16 18:56:31 -0400

    finishing backend

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'; 
+
+import Landing from './components/layout/Landing';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <h1>HEYYYYYYYYYYYYYY</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
